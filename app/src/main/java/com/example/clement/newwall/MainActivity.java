798177fa654ba04061bcef40521a4de96acae5ff
jefin <jeffinphilip@gmail.com>
2016-08-29 09:57:40 +0530

    simple json request

--- app/src/main/java/com/example/clement/newwall/MainActivity.java
+++ app/src/main/java/com/example/clement/newwall/MainActivity.java
@@ -14,17 +14,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    Button button2;
     ImageView imageView;
     String imgFrmServer = "http://192.168.1.4/one.jpg";
+    String json_url= "http://192.168.1.4/quey.php";
     TextView textView1;
     TextView textView2;
 
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         button= (Button)findViewById(R.id.btn);
+        button2= (Button)findViewById(R.id.btn2);
         imageView=(ImageView)findViewById(R.id.imageView);
         textView1=(TextView)findViewById(R.id.name);
         textView2=(TextView)findViewById(R.id.age);
@@ -64,6 +72,35 @@ public class MainActivity extends AppCompatActivity {
                 });
                 //requestQueue.add(imageRequest);
                 Singleton.getmInstance(MainActivity.this).addtorequestQueue(imageRequest);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.POST, json_url, (String)null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            textView1.setText(response.getString("Name"));
+                            textView2.setText(response.getString("Age"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(MainActivity.this,"some error occured",Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                });
+                Singleton.getmInstance(MainActivity.this).addtorequestQueue(jsonObjectRequest);
+
             }
         });
     }
